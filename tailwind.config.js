eee565b4d0d45a739fdf2e04c3182a1d27db0ac1
Simon <satmdeklerk@gmail.com>
2024-06-22 20:37:25 +0200

    Favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-        fontFamily: {
-            inter: ["Inter", "sans-serif"],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+            },
+        },
+        colors: {
+            darkblue: "#25116D",
+            green: "#32E335",
+            yellow: "#FBC31D",
+            white: "#F8FBFD",
+            gray: "#C9C9C9",
+            black: "#000000",
+        },
+        screens: {
+            xxs: "320px",
+            // => @media (min-width: 320px) { ... }
+
+            xs: "425px",
+            // => @media (min-width: 425px) { ... }
+
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            "2md": "898px",
+            // => @media (min-width: px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1361px",
+            // => @media (min-width: 1361px) { ... }
         },
     },
-    colors: {
-        darkblue: "#25116D",
-        green: "#32E335",
-        yellow: "#FBC31D",
-        white: "#F8FBFD",
-        gray: "#C9C9C9",
-        black: "#000000",
-    },
-    screens: {
-        xxs: "320px",
-        // => @media (min-width: 320px) { ... }
-
-        xs: "425px",
-        // => @media (min-width: 425px) { ... }
-
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
-
-        md: "768px",
-        // => @media (min-width: 768px) { ... }
-
-        "2md": "898px",
-        // => @media (min-width: px) { ... }
-
-        lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-
-        "2xl": "1361px",
-        // => @media (min-width: 1361px) { ... }
-    },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
