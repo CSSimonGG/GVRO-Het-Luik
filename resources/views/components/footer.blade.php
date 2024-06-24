fea89259383de848634b0879f82c7c0a66d4cad9
Simon <satmdeklerk@gmail.com>
2024-06-24 20:15:46 +0200

    Footer done

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,2 +1,37 @@
-<footer class="bg-darkblue text-white h-[256px]">
+<footer class="bg-darkblue text-white p-8 text-xl">
+    <div class="sm:px-[10%]">
+        <div class="lg:flex">
+            <div class="lg:mr-[25%]">
+                <ul class="lg:mb-[84px] max-lg:mb-4">
+                    <li class="font-bold">Summa College Restaurant</li>
+                    <li class="font-bold">Het Luik</li>
+                    <li>Sterrenlaan 10</li>
+                    <li>5631 KA Eindhoven</li>
+                </ul>
+                <span>© 2024 Summa College Het Luik</span>
+            </div>
+            <div class="flex max-lg:my-16">
+                <ul>
+                    <li class="font-bold">Openingstijden</li>
+                    <li>Maandag: </li>
+                    <li>Dinsdag: </li>
+                    <li>Woensdag: </li>
+                    <li>Donderdag: </li>
+                    <li>Vrijdag: </li>
+                    <li>Zaterdag: </li>
+                    <li>Zondag: </li>
+                </ul>
+                <ul>
+                    <br>
+                    <li>11:45-13:00</li>
+                    <li>11:45-13:00</li>
+                    <li>11:45-13:00</li>
+                    <li>11:45-13:00</li>
+                    <li>Gesloten</li>
+                    <li>Gesloten</li>
+                    <li>Gesloten</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -40,9 +40,6 @@ export default {
 
             xl: "1280px",
             // => @media (min-width: 1280px) { ... }
-
-            "2xl": "1361px",
-            // => @media (min-width: 1361px) { ... }
         },
     },
     plugins: [],
